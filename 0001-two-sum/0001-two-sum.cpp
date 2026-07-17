e8a32baaddc1949b5bb41882860f134d2aa40eee
RAVI  RANJAN <153702531+Ravi29102004@users.noreply.github.com>
2026-07-17 15:43:16 +0530

    Time: 0 ms (100%), Space: 14.8 MB (38.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -11,10 +11,20 @@ public:
         int start=0, end=nums.size()-1;
         while(start<end)
         {
-            int sum=arr[start].first+arr[end].first;
+            //before unsorted
+           // arr[i]	arr[i].first	arr[i].second
+//            arr[0]	3	            0
+//            arr[1]	2	            1
+//            arr[2]	4	            2
+           //after sortd
+           //arr[i]	arr[i].first	arr[i].second
+            //arr[0]	2	         1
+            //arr[1]	3	         0
+//             arr[2]	4	          2
+            int sum=arr[start].first+arr[end].first;  //sum=3+2 ussiko kahte hai arr[i].first=3,
             if(sum==target)
             {
-               return {arr[start].second,arr[end].second};
+               return {arr[start].second,arr[end].second};   //arr[i].second=1
             }
             else if(sum<target)
             {
